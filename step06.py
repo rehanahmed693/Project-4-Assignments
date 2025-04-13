aa92f46824976bd32ba58d29d167dc6a1d97266e
rehanahmed693 <rahijaan1999@gmail.com>
2025-04-13 05:36:51 +0500

    Delete step06.py

--- step06.py
+++
@@ -1,8 +0,0 @@
-def square():
-  print("This code is about square of given number")
-  num1:int = int(input("Enter any number and i will give u a square value. "))
-  print(f'The square of {num1} is {num1 ** 2}')
-
-if __name__ == "__main__":
-  square()
-     
