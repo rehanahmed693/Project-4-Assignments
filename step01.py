a3a35c6252926fc37460d2563d5597c893d6a525
rehanahmed693 <rahijaan1999@gmail.com>
2025-04-13 05:34:50 +0500

    Delete step01.py

--- step01.py
+++
@@ -1,9 +0,0 @@
-def add():
-    first_num = int(input("enter first number"))
-    second_num = int(input("enter second number"))
-    total = int (first_num + second_num)
-    print(total)
-
-add()
-     
-
