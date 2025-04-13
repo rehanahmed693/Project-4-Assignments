4c1a7d1982b72fb74affdf7b78095d5087123dfe
rehanahmed693 <rahijaan1999@gmail.com>
2025-04-13 06:19:11 +0500

    Add files via upload

---
+++ assignment-1 to 6/user-guess.py
@@ -0,0 +1,12 @@
+import random
+secret_number = random.randint(1, 10)
+print("The computer has thought of a secret number! Now, try to guess it!")
+while True:
+    user_guess = int(input("Enter your guess (1-10): "))
+    if user_guess == secret_number:
+        print("Bingo! You guessed it right! ✅")
+        break
+    elif user_guess > secret_number:
+        print("Think smaller!")
+    else:
+        print("Think bigger!")
