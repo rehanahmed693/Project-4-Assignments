746b5d638b220c3e5c842b3de20cbd423510b985
rehanahmed693 <rahijaan1999@gmail.com>
2025-04-13 05:35:36 +0500

    Delete step03.py

--- step03.py
+++
@@ -1,7 +0,0 @@
-def temp():
-    fahrenheit_degree = float(input("enter your fahrenheit degree "))
-    celsius_degree = (fahrenheit_degree - 32) * 5.0/9.0
-    print(f'temperature {fahrenheit_degree}f = {celsius_degree}c')
-
-if __name__ == "__main__" :
-    temp()   
