dc80a465e00588a6194f6ef6d782d88c845f6bb7
rehanahmed693 <rahijaan1999@gmail.com>
2025-04-13 05:24:46 +0500

    Add files via upload

---
+++ assignment-01-basics/step00.py
@@ -0,0 +1,16 @@
+PROMPT: str = "What do you want? "
+JOKE:str = "Why don’t skeletons fight each other? \n Because they don’t have the guts! 😆"
+SORRY:str = "Sorry I only tell jokes."
+
+def main():
+  user_input = input(PROMPT)
+  user_input = user_input.strip().lower()
+
+  if "joke" in user_input:
+    print(JOKE)
+  else:
+    print(SORRY)
+
+if __name__ == "__main__":
+  main()
+     
---
+++ assignment-01-basics/step04.py
@@ -0,0 +1,9 @@
+import random
+print("04_random_numbers")
+
+def main():
+  for _ in range(10):
+    print(random.randint(1,100),end = " ")
+
+if __name__ == "__main__":
+  main()
